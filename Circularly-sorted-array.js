41e95380d9a9862dda63301a971a9c13147f2f09
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-04 14:31:31 -0700

    Update Circularly-sorted-array.js

--- Circularly-sorted-array.js
+++ Circularly-sorted-array.js
@@ -23,3 +23,16 @@ Examples:
 
 
 // Solution
+
+function isCircleSorted(arr) {
+  let jumped = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[(i + 1) % arr.length]) {
+      if (jumped) {
+        return false;
+      }
+      jumped = true;
+    }
+  }
+  return true;
+}
